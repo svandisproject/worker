e5d4cf0e7ec5152ff0cae4e5fee907483ea64e86
rubilnik89 <rubilnik89@gmail.com>
2018-05-29 14:03:26 +0300

    api v2

--- crawlers/coinMarket.js
+++ crawlers/coinMarket.js
@@ -2,27 +2,28 @@ const config = require('../config');
 let instance = {
     execute(task, axios) {
             axios.get('https://api.coinmarketcap.com/v2/global')
-                .then(res => {
-                    let pages = Math.ceil(res.data.data.active_cryptocurrencies / 100);
-                    for(let n = 0; n < pages; n++){
-                        axios.get('https://api.coinmarketcap.com/v1/ticker?start=' + (n * 100))
-                        .then((res) => {
-                            res.data.map((coin) => {
+            .then(res => {
+                let pages = Math.ceil(res.data.data.active_cryptocurrencies / 100);
+                for(let n = 0; n < pages; n++){
+                    axios.get('https://api.coinmarketcap.com/v2/ticker?start=' + (n * 100))
+                    .then((res) => {
+                        for(let key in res.data.data) {
+                            if(res.data.data.hasOwnProperty(key)) {
+                                let coin = res.data.data[key];
                                 axios.post(`${config.API_URL}/api/asset`, {
                                     asset: {
                                         name: coin.name,
                                         symbol: coin.symbol,
                                         rank: coin.rank,
-                                        price_usd: coin.price_usd,
-                                        price_btc: coin.price_btc,
-                                        volume_usd_day: coin['24h_volume_usd'],
-                                        market_cap_usd: coin.market_cap_usd,
-                                        available_supply: coin.available_supply,
+                                        circulating_supply: coin.circulating_supply,
                                         total_supply: coin.total_supply,
                                         max_supply: coin.max_supply,
-                                        percent_change_hour: coin.percent_change_1h,
-                                        percent_change_day: coin.percent_change_24h,
-                                        percent_change_week: coin.percent_change_7d,
+                                        price_usd: coin.quotes.USD.price,
+                                        volume_usd_day: coin.quotes.USD.volume_24h,
+                                        market_cap_usd: coin.quotes.USD.market_cap,
+                                        percent_change_hour_usd: coin.quotes.USD.percent_change_1h,
+                                        percent_change_day_usd: coin.quotes.USD.percent_change_24h,
+                                        percent_change_week_usd: coin.quotes.USD.percent_change_7d,
                                         last_updated: coin.last_updated
                                     }
                                 })
@@ -32,10 +33,11 @@ let instance = {
                                 .catch((error) => {
                                     console.log(error.response.headers);
                                 })
-                            });
-                        });
-                    }
-                });
+                            }
+                        }
+                    });
+                }
+            });
     }
 };
 
