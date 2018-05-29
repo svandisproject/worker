0b9e99fec3189ad344fb75714124a1a3a45256c6
rubilnik89 <rubilnik89@gmail.com>
2018-05-29 14:24:05 +0300

    factory

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module['exports'] = {
-    API_URL: 'https://svandis-api.herokuapp.com',
+    API_URL: 'http://127.0.0.1:8000',
+    // API_URL: 'https://svandis-api.herokuapp.com',
     SOCKET_SERVER_URL: 'https://svandis-socket.herokuapp.com'
 };
--- crawlers/coinMarket.js
+++ crawlers/coinMarket.js
@@ -9,35 +9,38 @@ let instance = {
                     .then((res) => {
                         for(let key in res.data.data) {
                             if(res.data.data.hasOwnProperty(key)) {
-                                let coin = res.data.data[key];
-                                axios.post(`${config.API_URL}/api/asset`, {
-                                    asset: {
-                                        name: coin.name,
-                                        symbol: coin.symbol,
-                                        rank: coin.rank,
-                                        circulating_supply: coin.circulating_supply,
-                                        total_supply: coin.total_supply,
-                                        max_supply: coin.max_supply,
-                                        price_usd: coin.quotes.USD.price,
-                                        volume_usd_day: coin.quotes.USD.volume_24h,
-                                        market_cap_usd: coin.quotes.USD.market_cap,
-                                        percent_change_hour_usd: coin.quotes.USD.percent_change_1h,
-                                        percent_change_day_usd: coin.quotes.USD.percent_change_24h,
-                                        percent_change_week_usd: coin.quotes.USD.percent_change_7d,
-                                        last_updated: coin.last_updated
-                                    }
-                                })
-                                .then((resp) => {
-                                    console.log(resp.data);
-                                })
-                                .catch((error) => {
-                                    console.log(error.response.headers);
-                                })
+                                store(res.data.data[key]);
                             }
                         }
                     });
                 }
             });
+
+            function store(coin) {
+                axios.post(`${config.API_URL}/api/asset`, {
+                    asset: {
+                        name: coin.name,
+                        symbol: coin.symbol,
+                        rank: coin.rank,
+                        circulating_supply: coin.circulating_supply,
+                        total_supply: coin.total_supply,
+                        max_supply: coin.max_supply,
+                        price_usd: coin.quotes.USD.price,
+                        volume_usd_day: coin.quotes.USD.volume_24h,
+                        market_cap_usd: coin.quotes.USD.market_cap,
+                        percent_change_hour_usd: coin.quotes.USD.percent_change_1h,
+                        percent_change_day_usd: coin.quotes.USD.percent_change_24h,
+                        percent_change_week_usd: coin.quotes.USD.percent_change_7d,
+                        last_updated: coin.last_updated
+                    }
+                })
+                .then((resp) => {
+                    console.log(resp.data);
+                })
+                .catch((error) => {
+                    console.log(error.response.headers);
+                })
+            }
     }
 };
 
