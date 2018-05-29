431ce64be118120bb2519cc58be53632b6921e05
rubilnik89 <rubilnik89@gmail.com>
2018-05-29 14:25:31 +0300

    config

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module['exports'] = {
-    API_URL: 'http://127.0.0.1:8000',
-    // API_URL: 'https://svandis-api.herokuapp.com',
+    API_URL: 'https://svandis-api.herokuapp.com',
     SOCKET_SERVER_URL: 'https://svandis-socket.herokuapp.com'
 };
