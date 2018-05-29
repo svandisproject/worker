d2ddeb06d86db71ec8c208326b0cc4f49e47482c
rubilnik89 <rubilnik89@gmail.com>
2018-05-29 17:39:29 +0300

    factory

--- crawlers/coinMarket.js
+++ crawlers/coinMarket.js
@@ -1,3 +1,4 @@
+import {AssetFactory} from 'factory/AssetFactory';
 const config = require('../config');
 let instance = {
     execute(task, axios) {
@@ -9,38 +10,20 @@ let instance = {
                     .then((res) => {
                         for(let key in res.data.data) {
                             if(res.data.data.hasOwnProperty(key)) {
-                                store(res.data.data[key]);
+                                axios.post(`${config.API_URL}/api/asset`, {
+                                    asset: AssetFactory.buildAsset(res.data.data[key])
+                                })
+                                .then((resp) => {
+                                    console.log(resp.data);
+                                })
+                                .catch((error) => {
+                                    console.log(error.response.headers);
+                                })
                             }
                         }
                     });
                 }
             });
-
-            function store(coin) {
-                axios.post(`${config.API_URL}/api/asset`, {
-                    asset: {
-                        name: coin.name,
-                        symbol: coin.symbol,
-                        rank: coin.rank,
-                        circulating_supply: coin.circulating_supply,
-                        total_supply: coin.total_supply,
-                        max_supply: coin.max_supply,
-                        price_usd: coin.quotes.USD.price,
-                        volume_usd_day: coin.quotes.USD.volume_24h,
-                        market_cap_usd: coin.quotes.USD.market_cap,
-                        percent_change_hour_usd: coin.quotes.USD.percent_change_1h,
-                        percent_change_day_usd: coin.quotes.USD.percent_change_24h,
-                        percent_change_week_usd: coin.quotes.USD.percent_change_7d,
-                        last_updated: coin.last_updated
-                    }
-                })
-                .then((resp) => {
-                    console.log(resp.data);
-                })
-                .catch((error) => {
-                    console.log(error.response.headers);
-                })
-            }
     }
 };
 
