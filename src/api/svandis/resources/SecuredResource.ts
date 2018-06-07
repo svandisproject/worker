d3c0850131829fcb2f5afa1fa9486df35fa8ac42
codeforges <ruben@codeforges.com>
2018-06-07 11:43:10 +0200

    Fixed a issue with registration command

--- src/api/svandis/resources/SecuredResource.ts
+++ src/api/svandis/resources/SecuredResource.ts
@@ -1,7 +1,16 @@
 import {AxiosRequestConfig} from "@nestjs/common/http/interfaces/axios.interfaces";
+import {Logger} from "@nestjs/common";
 
 export class SecuredResource {
-    private token: string = require(process.env.PWD + '/runtime.json').token;
+    private token: string;
+
+    constructor() {
+        try {
+            this.token = require(process.env.PWD + '/runtime.json').token;
+        } catch (error) {
+            Logger.log('runtime not yet set');
+        }
+    }
 
     protected getSecuredRequestConfig(): AxiosRequestConfig {
         return {headers: {'X-WORKER-TOKEN': this.token}};
