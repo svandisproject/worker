c01fd0936664117f2685e68858a2f1f97b405f6f
codeforges <ruben@codeforges.com>
2018-08-21 14:49:41 +0200

    spawner adjustments

--- spawner.js
+++ spawner.js
@@ -1,13 +1,13 @@
-var respawn = require('respawn');
+let respawn = require('respawn');
 
-var monitor = respawn([__dirname + '/dist/main.js'], {
+let monitor = respawn([__dirname + '/dist/main.js'], {
     name: 'worker',
     env: {ENV_VAR: 'test'},
     cwd: '.',
     maxRestarts: -1,
     sleep: 1000,
     kill: 5000,
-    fork: true
+    fork: false
 });
 
 monitor.start();
